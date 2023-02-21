cae6072162cd0f52e8c2c5c7b19447ae224d891a
Gage Renzi <gage.renzi@healthstream.com>
2023-02-20 19:46:07 -0600

    initial pypi release

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
-# pymsasdax
-Small Python Module for running DAX queries against Microsoft Analysis Services, using COM Interop
+# About
+
+I wanted to consume some info from Azure Analysis Services from python and didn't see a convenient way to to do, so I wrote this. It should also work just fine with XMLA endpoints on Power BI Premium. 
+
+pymsasdax is a small Python Module for running DAX queries against Microsoft Analysis Services, using COM Interop. It does some basic typesniffing and returns a best guess Pandas Dataframe. 
+
+This does assume that the MSOLAP client is installed - you can get it from [here](https://learn.microsoft.com/en-us/analysis-services/client-libraries?view=asallproducts-allversions)
+
+I've done very little testing, so consider this alpha code. If you run into timeouts, make sure you're setting the timeout to an appropriate duration when creating the Connection. 
+
+tidy_column_names will remove brackets and replace spaces with underscores in the returned dataframe's columns. Set it to False in the Connection init if you don't want this behavior.
+
+Also, this is my first module up on pypi and I'm not exactly an expert on python, so feel free to submit an issue or a pull request. If I ended up reinventing the wheel here (ha!) and there was an easier way to do this, also please let me know.
+
+I hope you find this useful!
+
+## Python before 3.9
+This should actually work fine with for python 3 under 3.9. I've used this code for a couple of years now without incident -- I was just lazy when building this package. I **think** you'd need backports to support [dateparser](https://pypi.org/project/dateparser/).
+
+# Usage examples
+
+## Have an interactive prompt for Login to the resource
+```python
+from pymsasdax import dax
+
+with dax.Connection(
+        data_source='asazure://<region name>.asazure.windows.net/<instance here>,
+        initial_catalog='<my tabular database>'
+    ) as conn:
+    df = conn.query('EVALUATE ROW("a", 1)')
+    print(df)
+```
+
+## Use an app id
+```python
+from pymsasdax import dax
+
+with dax.Connection(
+        data_source='asazure://<region name>.asazure.windows.net/<instance here>,
+        initial_catalog='<my tabular database>'
+        uid='app:<client id>@<tenant id>',
+        password='<client secret>
+    ) as conn:
+    df = conn.query('EVALUATE ROW("a", 1)')
+    df.to_csv("raw_data.csv", index=False)        
+```
+
+# Dev Notes
+
+Current Version - 2023.1001-alpha
+## Tests
+
+Yes. There aren't any. Feel free to submit a PR. 
+
+## Building
+
+This might not be right but if you ever go to update pypi - 
+```
+pip-compile pyproject.toml
+bumpver update --minor
+python -m pip install -e . 
+#test
+python -m build
+twine check dist/*
+twine upload dist/*
+```
+
---
+++ src/pymsasdax/dax.py
@@ -0,0 +1,97 @@
+import clr
+import pandas as pd
+import dateparser
+
+
+class Connection:
+    def __init__(
+        self,
+        tidy_column_names=True,
+        conn_str=None,
+        initial_catalog=None,
+        data_source=None,
+        uid="",
+        password="",
+        timeout=30,
+    ):
+        if conn_str is not None:
+            self._connection_string = conn_str
+        else:
+            if not "initial_catalog" or not "data_source":
+                raise ValueError(
+                    f"initial_catalog and data_source must be specificed if not passing a connection string"
+                )
+            self._connection_string = f"Provider=MSOLAP;Persist Security Info=True;Initial Catalog={initial_catalog};Data Source={data_source};Timeout={timeout};UID={uid};Password={password}"
+
+        self._connection = None
+        self._tidy_column_names = tidy_column_names
+        clr.AddReference("System.Data")
+
+    def _handle_oledb_field(self, f):
+        mytype = str(type(f))
+        if mytype == "<class 'System.DBNull'>":
+            return None
+        if mytype == "<class 'int'>":
+            return int(f)
+        if mytype == "<class 'System.Decimal'>":
+            return float(f.ToString())
+        if mytype == "<class 'float'>":
+            return float(f)
+        if mytype == "<class 'str'>":
+            return str(f)
+        if mytype == "<class 'System.DateTime'>":
+            return dateparser.parse(str(f))
+        if mytype == "<class 'bool'>":
+            return f
+        raise Exception("Unknown Type " + mytype)
+
+    def _tidy_col_name(self, c):
+        newname = c.replace("[", "_").replace("]", "_").replace(" ", "_")
+        return newname.strip("_")
+
+    def __enter__(self):
+        # don't be lazy - force connection if we're entering with with
+        if self._connection is None:
+            import System.Data.OleDb as ADONET
+
+            self._connection = ADONET.OleDbConnection(self._connection_string)
+            self._connection.Open()
+            return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self._connection.Close()
+
+    def query(self, daxcmd):
+        # lazy connection
+        if self._connection is None:
+            import System.Data.OleDb as ADONET
+
+            self._connection = ADONET.OleDbConnection(self._connection_string)
+            self._connection.Open()
+
+        command = self._connection.CreateCommand()
+        command.CommandText = daxcmd
+        reader = command.ExecuteReader()
+        schema_table = reader.GetSchemaTable()
+
+        # schema_table rows are query result columns
+        columns = []
+        for r in schema_table.Rows:
+            columns.append(r["ColumnName"])
+
+        rows = [None] * reader.get_RecordsAffected()
+        for x in range(0, reader.get_RecordsAffected()):
+            reader.Read()
+            # https://docs.microsoft.com/en-us/dotnet/api/system.typecode?view=netframework-4.7.2
+            # but ints dont have them
+            # rows[x] = list( [ reader[c] if reader[c].GetTypeCode() != 2 else None for c in columns] )
+            rows[x] = list([self._handle_oledb_field(reader[c]) for c in columns])
+        df = pd.DataFrame.from_records(
+            data=rows,
+            coerce_float=True,
+        )
+        if self._tidy_column_names:
+            df.rename(columns={c: self._tidy_col_name(c) for c in columns})
+        del rows
+        del reader
+        return df
